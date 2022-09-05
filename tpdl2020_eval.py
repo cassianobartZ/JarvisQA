590a862703ebfbe4e8615db980d482689aaa95e0
Cassiano Bartz <cassiano.bartz@outlook.com>
2022-09-05 19:27:24 -0300

    add some benchmarks

--- tpdl2020_eval.py
+++ tpdl2020_eval.py
@@ -33,9 +33,7 @@ def exp1(top_k=10, positions=None):
 
 
 def exp2(top_k=10, positions=None):
-# def exp2(top_k=1, positions=None):
     if positions is None:
-        # positions = [1, 10]
         positions = [1, 3, 5, 10]
     ds_path = './datasets/sciqa-mod/SciQA-DS.csv'
     final_result = ''
@@ -43,9 +41,8 @@ def exp2(top_k=10, positions=None):
     #               'bert-large-cased-whole-word-masking-finetuned-squad', 'deepset/bert-base-cased-squad2',
     #               'deepset/bert-large-uncased-whole-word-masking-squad2', 'distilbert-base-uncased-distilled-squad',
     #               'ktrapeznikov/albert-xlarge-v2-squad-v2', 'replydotai/albert-xxlarge-v1-finetuned-squad2']:
-    with open('benchmark-results-exp2-split-test-bert-lcs.txt', 'w+') as out_file:
-        # for model in ['ktrapeznikov/albert-xlarge-v2-squad-v2']:
-        for model in ['bert-large-cased-whole-word-masking-finetuned-squad']:
+    with open('benchmark-results-exp2-all-split-test.txt', 'w+') as out_file:
+        for model in ['bert-large-cased-whole-word-masking-finetuned-squad', 'ktrapeznikov/albert-xlarge-v2-squad-v2']:
             final_result = f'{final_result}On model: {model}\n'
             print(f'Starting with model {model}')
             for kind in ['normal', 'aggregation', 'related', 'similar']:
