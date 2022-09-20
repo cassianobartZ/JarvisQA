86667530d065b6be2589e90432129c67f466076e
Cassiano Bartz <cassiano.bartz@outlook.com>
2022-09-20 20:17:44 -0300

    add sum - split most common - add highest and lowest - split extra_info if bigger than 500 tokens

--- t2t.py
+++ t2t.py
@@ -40,13 +40,16 @@ class T2T:
                 max_value = column_df.max()
                 min_value = column_df.min()
                 avg_value = column_df.mean()
+                sum = column_df.sum()
                 info.append(f'The maximum value of {column} is {max_value},'
                             f' the minimum value of {column} is {min_value},'
-                            f' and the average value of {column} is {avg_value:.2f}')
-                max_title = df.iloc[column_df.argmax()]['Title']
-                min_title = df.iloc[column_df.argmin()]['Title']
-                info.append(f'The paper with the maximum {column} is "{max_title}"'
-                            f' and the paper with the minimum {column} is {min_title}')
+                            f' and the average value of {column} is {avg_value:.2f},'
+                            f'the total of {column} is {sum}')
+                # max_title = df.iloc[column_df.argmax()]['Title']
+                # min_title = df.iloc[column_df.argmin()]['Title']
+                # info.append(f'The paper with the maximum {column} is "{max_title}"'
+                #             f' and the paper with the minimum {column} is {min_title}')
+                self.append_text_for_maximum_and_minimum_numeric_column(info, column, df)
                 self.append_text_for_numeric_column(info, column, df)
             if is_string_dtype(df[column]):
                 column_df = df[column].fillna('')
@@ -60,10 +63,41 @@ class T2T:
                 min_occurrence = min(counts.values())
                 most_common = [k for k, v in counts.items() if v == max_occurrence]
                 least_common = [k for k, v in counts.items() if v == min_occurrence]
-                info.append(f'The most common {column} {self.append_value(", ".join(most_common))},'
-                            f' and the least common {self.append_value(", ".join(least_common))}')
+
+                for columnValue in most_common:
+                    info.append(f'The most common {column} {self.append_value(columnValue)}')
+
+                for columnValue in least_common:
+                    info.append(f'The least common {column} {self.append_value(columnValue)}')
+
+                # # identified error: least common or most common can grab a lot of entries and by itself explode 500 tokens
+                # info.append(f'The most common {column} {self.append_value(most_common[0])},'
+                #             f' and the least common {self.append_value(least_common[0])}')
         return '\n'.join(info)
+
     
+    def append_text_for_maximum_and_minimum_numeric_column(self, info: str, numericColumnName: str, df: pd.DataFrame) -> str:
+        columns = list(df)
+        column_df = df[numericColumnName].fillna(0)
+        rowForHighestNumericValue = df.iloc[column_df.argmax()]
+        rowForLowestNumericValue = df.iloc[column_df.argmin()]
+
+        # info.append(f'The paper with the maximum {column} is "{max_title}"'
+        #             f' and the paper with the minimum {column} is {min_title}')
+
+        #  the {iterationColum/title} with the highest {numericColumn/numberOfSpecies} is {iterationColumnValue/the invertebrale...}
+        #  {iterationColumn} {numericColumnName} {iterationColumnValue}
+
+        for indexIterationColumn,iterationColumn in enumerate(columns):
+            if is_numeric_dtype(df[iterationColumn]):
+                continue
+            lowestRowIterationColumnValue = rowForLowestNumericValue[iterationColumn]
+            highestRowIterationColumnValue = rowForHighestNumericValue[iterationColumn]
+            info.append(f'The {iterationColumn} with the highest {numericColumnName} is {highestRowIterationColumnValue}')
+            info.append(f'The {iterationColumn} with the lowest {numericColumnName} is {lowestRowIterationColumnValue}')
+            
+        return info
+        
     def append_text_for_numeric_column(self, info: str, numericColumnName: str, df: pd.DataFrame) -> str:
         columns = list(df)
 
@@ -97,24 +131,52 @@ class T2T:
         extra_info = self.append_aggregation_info(pd.read_csv(csv_path, sep=sep))
 
         #  maybe make a branch here, if extra_info is larger than 500 or not, and handle it diffently
+        if len(extra_info.split()) > 500:
+            extra_info = self.splitExtraInfoInArraysOfSuitableSize(extra_info)
+        else:
+            extra_info = [extra_info]
 
+        # okay so I have extra_info split but now I have to deal with the context itself, maybe its better to separate text from extra info in runs?
         resultArray = []
         allText = ''
         for row in rows:
             rowText = self.row_2_text(row, header, empty)
-            test = allText + '\n' + rowText + '\n' + extra_info
-            if len(test.split()) > 512:
-                resultArray.append(allText+ '\n' + extra_info)
+            test = allText + '\n' + rowText + '\n'
+            if len(test.split()) > 500:
+                extra_info.append(allText+ '\n')
                 allText = rowText
                 continue
             allText = allText + '\n' + rowText
+        extra_info.append(allText)
 
-        if len(resultArray) == 0:
-            resultArray.append(allText+ '\n' + extra_info)
+        if extra_info[0] == '':
+            print('hey')
+
+        if len(extra_info) == 2 and len((extra_info[0]+extra_info[1]).split()) < 500:
+            return [(extra_info[0]+extra_info[1])]
         else:
-            resultArray[-1] = resultArray[-1] + '\n' + extra_info
+            # attention here, i might have fucked up and never appended the last string
+            # might be cool to make a function which combines all the chunks in the maximum size
+            # resultArray[-1] = resultArray[-1] + '\n' + extra_info
+            return extra_info
 
-        return resultArray
+    def splitExtraInfoInArraysOfSuitableSize(self, extra_info):
+        lineBreaksIndexesForCorrectSplit = [pos for pos, char in enumerate(extra_info) if char == '\n']
+        numberOfArrays = 1
+
+        startingIndexForComparisson = 0
+        lastCharBelowCeiling = 0
+        returningArrays = []
+        for pos, char in enumerate(lineBreaksIndexesForCorrectSplit):
+            if len(extra_info[startingIndexForComparisson:char].split()) < 500:
+                lastCharBelowCeiling = char
+                continue
+            else:
+                returningArrays.append(extra_info[startingIndexForComparisson:lastCharBelowCeiling])
+                numberOfArrays = numberOfArrays+1
+                startingIndexForComparisson = lastCharBelowCeiling+1
+        returningArrays.append(extra_info[startingIndexForComparisson:])
+        return returningArrays
 
 
 if __name__ == '__main__':
